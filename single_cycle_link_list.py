49b6bee7ad9c3dddd30c16c2f852f4e976d566f0
赵文浩 <1050986935@qq.com>
2021-12-27 00:07:04 +0800

    python单项循环链表

--- single_cycle_link_list.py
+++ single_cycle_link_list.py
@@ -6,8 +6,11 @@ class Node(object):
 
 
 class SingLeLinkList(object):
+    """单向循环链表"""
     def __init__(self, node=None):
         self.__head = node
+        if node:
+            node.next = node
 
     def is_empty(self):
         """链表是否为空"""
@@ -15,37 +18,53 @@ class SingLeLinkList(object):
 
     def length(self):
         """链表长度"""
+        if self.is_empty():
+            return 0
         # 定义游标来遍历链表
         cur = self.__head
         # 定义长度
-        count = 0
-        while cur != None:
+        count = 1
+        while cur.next != self.__head:
             count += 1
             cur = cur.next
         return count
 
     def travel(self):
         """遍历整个链表"""
+        if self.is_empty():
+            return
         cur = self.__head
-        while cur != None:
+        while cur.next is not self.__head:
             print(cur.elem, end=" ")
             cur = cur.next
+        print(cur.elem)
 
     def add(self, item):
         """链表头部添加元素(头插法)"""
         node = Node(item)
-        node.next = self.__head
-        self.__head = node
+        if self.is_empty():
+            self.__head = node
+            node.next = node
+        else:
+            cur = self.__head
+            while cur.next is not self.__head:
+                cur = cur.next
+            # 退出循环，cur指向尾结点
+            node.next = self.__head
+            self.__head = node
+            cur.next = node
 
     def append(self, item):
         """链表尾部添加元素"""
         node = Node(item)
+        node.next = node
         if self.is_empty():
             self.__head = node
         else:
             cur = self.__head
-            while cur.next != None:
+            while cur.next is not self.__head:
                 cur = cur.next
+            node.next = self.__head
             cur.next = node
 
     def insert(self, pos, item):
@@ -67,27 +86,47 @@ class SingLeLinkList(object):
 
     def remove(self, item):
         """删除节点"""
+        if self.is_empty():
+            return
         cur = self.__head
         pre = None
-        while cur != None:
+        while cur.next is not self.__head:
             if cur.elem == item:
                 if cur == self.__head:
+                    # 头结点
+                    # 先找尾结点
+                    rear = self.__head
+                    while rear.next is not self.__head:
+                        rear = rear.next
                     self.__head = cur.next
+                    rear.next = self.__head
                 else:
+                    # 中间结点
                     pre.next = cur.next
-                break
+                return
             else:
                 pre = cur
                 cur = cur.next
+        # 退出循环，cur指向尾结点
+        if cur.elem == item:
+            if cur is self.__head:
+                # 链表只有一个结点
+                self.__head = None
+            else:
+                pre.next = cur.next
 
     def search(self, item):
         """查找节点是否存在"""
+        if self.is_empty():
+            return False
         cur = self.__head
-        while cur != None:
+        while cur.next is not self.__head:
             if cur.elem == item:
                 return True
             else:
                 cur = cur.next
+        if cur.elem is item:
+            return True
         return False
 
 
@@ -95,19 +134,12 @@ if __name__ == "__main__":
     ll = SingLeLinkList()
     print(ll.is_empty())
     print(ll.length())
-
     ll.append(1)
-    print(ll.is_empty())
-    print(ll.length())
-
-
-
-    ll.append(2)
-
-    ll.insert(2, 30)
-    ll.insert(10, 200)
-    ll.remove(200)
-    ll.insert(-1, 20)
-    #ll.add(8)
+    ll.add(8)
     ll.append(3)
+    ll.append(4)
+    ll.append(5)
+    ll.append(6)
+    ll.travel()
+    ll.insert(10, 200)
     ll.travel()
