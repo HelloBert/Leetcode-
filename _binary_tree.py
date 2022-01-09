d481d201927e51e46a25812afb3bd13722b9997c
赵文浩 <1050986935@qq.com>
2022-01-09 23:30:44 +0800

    二叉树实现先序排序，中序排序，倒序排序

--- _binary_tree.py
+++ _binary_tree.py
@@ -31,5 +31,63 @@ class Tree(object):
             else:
                 queue.append(cur_node.rchild)
 
-tree = Tree()
+    def breadth_travel(self):
+        """广度遍历"""
+        if self.root is None:
+            return
+        queue = [self.root]
+        while queue:
+            cur_node = queue.pop(0)
+            print(cur_node.elem, end=" ")
+            if cur_node.lchild is not None:
+                queue.append(cur_node.lchild)
+            if cur_node.rchild is not None:
+                queue.append(cur_node.rchild)
+
+    def preorder(self, node):
+        """先序遍历"""
+        if node is None:
+            return
+        print(node.elem, end=" ")
+        self.preorder(node.lchild)
+        self.preorder(node.rchild)
+
+    def inorder(self, node):
+        """中序遍历"""
+        if node is None:
+            return
+        self.inorder(node.lchild)
+        print(node.elem, end=" ")
+        self.inorder(node.rchild)
+
+    def postorder(self, node):
+        """后续遍历"""
+        if node is None:
+            return
+        self.postorder(node.lchild)
+        self.postorder(node.rchild)
+        print(node.elem, end=" ")
+
+
+if __name__ == "__main__":
+    tree = Tree()
+    tree.add(0)
+    tree.add(1)
+    tree.add(2)
+    tree.add(3)
+    tree.add(4)
+    tree.add(5)
+    tree.add(6)
+    tree.add(7)
+    tree.add(8)
+    tree.add(9)
+    tree.breadth_travel()
+    print(" ")
+    tree.preorder(tree.root)
+    print(" ")
+    tree.inorder(tree.root)
+    print(" ")
+    tree.postorder(tree.root)
+    print(" ")
+
 
